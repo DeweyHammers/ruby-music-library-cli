500a1f9c398b7c893aca657a47122e192aec7c94
Dewey <hammersdrichard@gmail.com>
2021-01-15 05:53:43 -0800

    Done.

--- lib/music_libray_controller.rb
+++ lib/music_libray_controller.rb
@@ -1,4 +1,6 @@
 class MusicLibraryController
+    extend Concerns::Findable
+
     def initialize(path='./db/mp3s')
         MusicImporter.new(path).import
     end
@@ -18,8 +20,23 @@ class MusicLibraryController
 
     def get_input
         input = gets.chomp
-        if input != "exit"
-          get_input()  
+        case input
+        when "list songs"
+            list_songs()
+        when "list artists"
+            list_artists()
+        when "list genres"
+            list_genres()
+        when "list artist"
+            list_songs_by_artist()
+        when "list genre"
+            list_songs_by_genre()
+        when "play song"
+            play_song()
+        else
+            if input != "exit"
+                get_input()  
+            end
         end
     end
 
@@ -55,6 +72,44 @@ class MusicLibraryController
     end
 
     def list_songs_by_artist
+        puts "Please enter the name of an artist:"
+        input = gets.chomp
+        match = []
+        num = 0
+        songs = Song.all.sort_by {|song| song.name} 
+        songs.map {|song| match << song if song.artist.name == input}
+        match.map do |song|
+            num += 1 
+            puts "#{num}. #{song.name} - #{song.genre.name}"
+        end
+    end
 
+    def list_songs_by_genre
+        puts "Please enter the name of a genre:"
+        input = gets.chomp
+        match = []
+        num = 0
+        songs = Song.all.sort_by {|song| song.name}
+        songs.map {|song| match << song if song.genre.name == input}
+        match.map do |song|
+            num += 1 
+            puts "#{num}. #{song.artist.name} - #{song.name}"
+        end
+    end
+
+    def play_song
+        puts "Which song number would you like to play?"
+        input = gets.chomp
+        num = 0
+        list = {}
+        match = nil
+        songs = Song.all.sort_by {|song| song.name}
+        songs.map do |song|
+            num += 1 
+            list[num] = "Playing #{song.name} by #{song.artist.name}"
+        end
+        list.map  {|keys, values| keys == input.to_i ? match = values : false}
+        puts match if match != nil
     end
 end
+
